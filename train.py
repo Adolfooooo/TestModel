9a149dcbd972e370e31e332e731958583bb909ba
Adolfooooo <xuhaolxy@gmail.com>
2025-01-16 16:26:12 +0800

    LeNet, third commit, structure change

--- train.py
+++ train.py
@@ -3,41 +3,8 @@ import torch.nn as nn
 import torch.optim as optim
 from data.datasets.ACDC.ACDCDataset import ACDCDataset
 from data.Dataloader import get_dataloader
-
-
+from model.LeNet import LeNet
 import sys
-
-
-class LeNet(nn.Module):
-    def __init__(self, in_channels, out_channels_list=[6, 12], kernel_size=5, stride=1, padding=2):
-        super(LeNet, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels = in_channels, out_channels=out_channels_list[0], kernel_size=kernel_size, stride=stride, padding=padding),
-            # nn.BatchNorm2d(num_features=out_channels_list[0]),
-            nn.Sigmoid(),
-        )
-        self.avgpool1 = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels=out_channels_list[0], out_channels=out_channels_list[1], kernel_size=kernel_size),
-            # nn.BatchNorm2d(num_features=out_channels_list[1]),
-            nn.Sigmoid(),
-        )
-        self.avgpool2 = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.fc = nn.Sequential(
-            nn.Flatten(start_dim=1, end_dim=-1),
-            nn.Linear(12*5*5, out_features=120),
-            nn.Sigmoid(),
-            nn.Linear(120, out_features=84),
-            nn.Sigmoid(),
-            nn.Linear(84, out_features=10)
-        )
-        
-    
-    def forward(self, x):
-        front = self.avgpool2(self.conv2(self.avgpool1(self.conv1(x))))
-        
-        return self.fc(front)
-
 
 
 def init_weights(m):
